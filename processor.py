d345371a8838a19aa8975aa82d52d462129ff328
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2024-06-20 14:37:26 +0100

    Add support for different versions and validation-only

--- processor.py
+++ processor.py
@@ -12,13 +12,17 @@ def main():
     config.init()
 
     args = parse_args()
-    table1, table2, table3, table4 = args.table1, args.table2, args.table3, args.table4
+    table1, table2, table3, table4, version, check = args.table1, args.table2, args.table3, args.table4, args.version, args.check
 
     # Default names of Monocle Table and Data JSON
     table_monocle = 'table_monocle.csv'
     data = 'data.json'
 
-    validator.validate(table1, table2, table3)
+    validator.validate(table1, table2, table3, version)
+
+    if check:
+        return
+
     get_csv.get_table4(table1, table3, table4)
     get_csv.get_monocle(table1, table2, table3, table4, table_monocle)
     get_json.get_data(table_monocle, data)
@@ -30,51 +34,70 @@ def main():
 def parse_args():
     parser = argparse.ArgumentParser(
         allow_abbrev=False,
-        description='Process GPS (Global Pneumococcal Sequencing Project) database updates. This tool validates table1 (metadata), table2 (qc) and table3 (analysis) provided by the user; then generates table4, Monocle Table and Data JSON of the GPS Database Overview.',
-        epilog='If you have updated any files (except api_keys.py) in the "data" directory, please submit a PR to https://github.com/sanger-bentley-group/gps-database-processor'
+        description='Process GPS (Global Pneumococcal Sequencing Project) database updates. This tool validates table1 (Metadata), table2 (QC) and table3 (Analysis) provided by the user; then generates table4, Monocle Table and Data JSON of the GPS Database Overview.',
+        epilog='If you have updated any files (except api_keys.py) in the "data" directory, please submit a PR to https://github.com/sanger-bentley-group/gps-database-processor',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
     parser.add_argument(
         '-m', '--meta',
         dest='table1',
-        metavar='your_file_name.csv',
         default='table1.csv',
-        help='(Input file) Override the default table1 (metadata) file name of table1.csv'
+        help='file name of table1 metadata'
     )
 
     parser.add_argument(
         '-q', '--qc',
         dest='table2',
-        metavar='your_file_name.csv',
         default='table2.csv',
-        help='(Input file) Override the default table2 (qc) file name of table2.csv'
+        help='file name of table2 QC'
     )
 
     parser.add_argument(
         '-a', '--analysis',
         dest='table3',
-        metavar = 'your_file_name.csv',
         default='table3.csv',
-        help='(Input file) Override the default table3 (analysis) file name of table3.csv'
+        help='file name of table3 analysis'
     )
 
     parser.add_argument(
         '-o', '--output',
         dest='table4',
-        metavar = 'your_file_name.csv',
         default='table4.csv',
-        help='(Output file) Override the default table4 file name of table4.csv'
+        help='file name of table4'
+    )
+
+    parser.add_argument(
+        '-v', '--version',
+        dest='version',
+        default='1',
+        metavar='N',
+        help='version of the GPS database'
+    )
+
+    parser.add_argument(
+        '-c', '--check',
+        action="store_true",
+        help='perform validation only'
     )
 
     args = parser.parse_args()
 
     # Check all input and output files are .csv
     non_csv = []
+
     for arg in vars(args):
-        filename = getattr(args, arg)
-        if filename[-4:] == '.csv':
-            continue
-        non_csv.append(filename)
+        match arg:
+            case "version":
+                version = getattr(args, arg)
+                if version not in ("1", "2"):
+                    config.LOG.critical(f'This tool only work with GPS Database version 1 and 2. Please select a compatible version number.')
+                    sys.exit(1)
+            case str(x) if x.startswith("table"):
+                filename = getattr(args, arg)
+                if filename[-4:] != '.csv':
+                    non_csv.append(filename)
+    
     if non_csv:
         config.LOG.critical(f'The following file name(s) do not have ".csv" as their file extension: {", ".join(non_csv)}. This tool only work with ".csv" files. The process will now be halted.')
         sys.exit(1)
