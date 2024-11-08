303f764d82d9d94359f62ec9ffe0f3b2d0456bd0
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2024-11-08 14:15:57 +0000

    Initial work on table3 data generation, add GPSC and serotype colours

--- scripts/add_gps_pipeline_output.py
+++ scripts/add_gps_pipeline_output.py
@@ -9,10 +9,10 @@ import os
 def main():
     args = parse_arguments()
     
-    df_results, df_info, table2_path, table3_path, df_table2, df_table3 = args_check(args)
+    df_results, df_info, table2_path, table3_path, df_table2, df_table3, df_gpsc_colour, df_serotype_colour = args_check(args)
     
     df_table2_new_data = generate_table2_data(df_results, df_info, args.assembler)
-    df_table3_new_data = generate_table3_data(df_results, df_info)
+    df_table3_new_data = generate_table3_data(df_results, df_info, df_gpsc_colour, df_serotype_colour)
 
     integrate_table2(df_table2_new_data, df_table2, table2_path)
 
@@ -28,7 +28,7 @@ def args_check(args):
         df_info = pd.read_csv(args.info, dtype=str, keep_default_na=False)
         
         if (missing_laneids := (set(df_results["Sample_ID"]) - set(df_info["Lane_id"]))):
-            sys.exit(f"Error: Information of the following Lane ID(s) are not provided: {', '.join(missing_laneids)}")
+            sys.exit(f"Error: Information of the following Lane ID(s) are not provided: {', '.join(sorted(missing_laneids))}")
 
         optional_columns = ["Supplier_name", "Sanger_sample_id", "ERR", "ERS"]
         for col in optional_columns:
@@ -51,7 +51,17 @@ def args_check(args):
     except FileNotFoundError:
         sys.exit(f"Error: table2.csv and/or table3.csv are not found in {args.data}!")
 
-    return df_results, df_info, table2_path, table3_path, df_table2, df_table3
+    try:
+        df_gpsc_colour = pd.read_csv(args.gpsccolour, dtype=str, keep_default_na=False)
+    except FileNotFoundError:
+        sys.exit(f"Error: {args.gpsccolour} is not found!")
+    
+    try:
+        df_serotype_colour = pd.read_csv(args.serotypecolour, dtype=str, keep_default_na=False)
+    except FileNotFoundError:
+        sys.exit(f"Error: {args.serotypecolour} is not found!")
+
+    return df_results, df_info, table2_path, table3_path, df_table2, df_table3, df_gpsc_colour, df_serotype_colour
 
 
 # Generate table2 data for integration
@@ -85,9 +95,82 @@ def generate_table2_data(df_results, df_info, assembler):
     return df_table2_new_data
 
 
-def generate_table3_data(df_results, df_info):
+def generate_table3_data(df_results, df_info, df_gpsc_colour, df_serotype_colour):
     df_table3_new_data = df_results[df_results["Overall_QC"] == "PASS"].copy()
-    pass
+
+    df_table3_new_data = df_table3_new_data.merge(df_info, left_on="Sample_ID", right_on="Lane_id", how="left")
+
+    # Add legacy column
+    legacy_columns = ["WGS_SYN", "WGS_SYN_SIR", "WGS_LZO", "WGS_LZO_SIR", "EC"]
+    for col in legacy_columns:
+        df_table3_new_data[col] = "_"
+
+    # Rename columns that are not in table2 format
+    df_table3_new_data.rename(
+        columns = {
+            "ST": "In_silico_ST",
+            "Serotype": "In_silico_serotype"
+        },
+        inplace=True
+    )
+
+
+    # Check all GPSCs have colours assigned, then assign those colours 
+    if gpsc_no_colour := (set(df_table3_new_data["GPSC"]) - set(df_gpsc_colour["GPSC"])):
+        sys.exit(f"Error: The following GPSC(s) are not found in the selected GPSC colour assignment file: {', '.join(sorted(gpsc_no_colour))}")
+    df_table3_new_data["GPSC__colour"] = df_table3_new_data["GPSC"].map(df_gpsc_colour.set_index("GPSC")["GPSC__colour"])
+
+    # Strip leading 0 and convert to all UPPER from GPS Pipeline result
+    # Check all serotypes have colours assigned, then assign those colours 
+    df_table3_new_data["In_silico_serotype"] = df_table3_new_data["In_silico_serotype"].str.replace(r"^0+", "", regex=True).str.upper()
+    if serotype_no_colour := (set(df_table3_new_data["In_silico_serotype"]) - set(df_serotype_colour["In_silico_serotype"])):
+        sys.exit(f"Error: The following serotype(s) are not found in the selected serotype colour assignment file: {', '.join(sorted(serotype_no_colour))}")
+    df_table3_new_data["In_silico_serotype__colour"] = df_table3_new_data["In_silico_serotype"].map(df_serotype_colour.set_index("In_silico_serotype")["In_silico_serotype__colour"])
+
+    # Extract and reorder relevant columns
+    # No_of_genome and Duplicate columns will be inserted in integrate_table3 function
+    df_table3_new_data = df_table3_new_data[[
+        "Lane_id", "Public_name", "Sanger_sample_id", "ERR", "ERS", 
+        "In_silico_ST", "aroE", "gdh", "gki", "recP", "spi", "xpt", "ddl", 
+        "GPSC", "GPSC__colour", 
+        "In_silico_serotype", "In_silico_serotype__colour", 
+        # "pbp1a", "pbp2b", "pbp2x", 
+        # "WGS_PEN", "WGS_PEN_SIR_Meningitis", "WGS_PEN_SIR_Nonmeningitis", 
+        # "WGS_AMO", "WGS_AMO_SIR", 
+        # "WGS_MER", "WGS_MER_SIR", 
+        # "WGS_TAX", "WGS_TAX_SIR_Meningitis", "WGS_TAX_SIR_Nonmeningitis", 
+        # "WGS_CFT", "WGS_CFT_SIR_Meningitis", "WGS_CFT_SIR_Nonmeningitis", 
+        # "WGS_CFX", "WGS_CFX_SIR", 
+        # "WGS_ERY", "WGS_ERY_SIR", 
+        # "WGS_CLI", "WGS_CLI_SIR", 
+        # "WGS_SYN", "WGS_SYN_SIR", 
+        # "WGS_LZO", "WGS_LZO_SIR", 
+        # "WGS_ERY_CLI", 
+        # "WGS_COT", "WGS_COT_SIR", 
+        # "WGS_TET", "WGS_TET_SIR", 
+        # "WGS_DOX", "WGS_DOX_SIR", 
+        # "WGS_LFX", "WGS_LFX_SIR", 
+        # "WGS_CHL", "WGS_CHL_SIR", 
+        # "WGS_RIF", "WGS_RIF_SIR", 
+        # "WGS_VAN", "WGS_VAN_SIR", 
+        # "EC", 
+        # "Cot", 
+        # "Tet__autocolour", 
+        # "FQ__autocolour", 
+        # "Other", 
+        # "PBP1A_2B_2X__autocolour", 
+        # "WGS_PEN_SIR_Meningitis__colour", "WGS_PEN_SIR_Nonmeningitis__colour", "WGS_AMO_SIR__colour", "WGS_MER_SIR__colour", "WGS_TAX_SIR_Meningitis__colour", "WGS_TAX_SIR_Nonmeningitis__colour", "WGS_CFT_SIR_Meningitis__colour", "WGS_CFT_SIR_Nonmeningitis__colour", "WGS_CFX_SIR__colour", "WGS_ERY_SIR__colour", "WGS_CLI_SIR__colour", "WGS_SYN_SIR__colour", "WGS_LZO_SIR__colour", "WGS_COT_SIR__colour", "WGS_TET_SIR__colour", "WGS_DOX_SIR__colour", "WGS_LFX_SIR__colour", "WGS_CHL_SIR__colour", "WGS_RIF_SIR__colour", "WGS_VAN_SIR__colour", 
+        # "ermB", "ermB__colour", 
+        # "mefA", "mefA__colour", 
+        # "folA_I100L", "folA_I100L__colour", 
+        # "folP__autocolour", 
+        # "cat", "cat__colour"
+    ]]
+
+    # To-Remove
+    df_table3_new_data.to_csv("table3_test.csv", index=False)    
+
+    return df_table3_new_data
 
 
 def integrate_table2(df_table2_new_data, df_table2, table2_path):
@@ -104,6 +187,8 @@ def parse_arguments():
         description='Integrate GPS Pipeline results into gps2-data compatible table2.csv and table3.csv',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
+
+    base_path = os.path.abspath(os.path.dirname(sys.argv[0]))
 
     parser.add_argument(
         '-r', '--results',
@@ -127,6 +212,18 @@ def parse_arguments():
         '-a', '--assembler',
         default='SHOVILL',
         help='de novo assembler used in the run'
+    )
+
+    parser.add_argument(
+        '-g', '--gpsccolour',
+        default=os.path.join(base_path, "data/gpsc_colours.csv"),
+        help='path to GPSC colour assignment file'
+    )
+    
+    parser.add_argument(
+        '-s', '--serotypecolour',
+        default=os.path.join(base_path, "data/serotype_colours.csv"),
+        help='path to serotype colour assignment file'
     )
 
     return parser.parse_args()
